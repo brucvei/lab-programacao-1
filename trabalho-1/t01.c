06481fa6a8cf921cd9a85d46b43b143e3512a780
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2022-10-13 14:19:37 -0300

    ajeitei o intervalo

--- trabalho-1/t01.c
+++ trabalho-1/t01.c
@@ -8,7 +8,7 @@
 int main()
 {
     setlocale(LC_ALL, "portuguese");
-    int opcao, numero, param1, param2;
+    int opcao, numero, param1, param2, p1Anterior, p2Anterior;
 
     param1 = 0;
     param2 = 100;
@@ -17,7 +17,12 @@ int main()
     do
     {
         srand(time(NULL));
-        numero = param1 + (rand() % param2 + 1);
+        
+        p1Anterior = param1;
+        p2Anterior = param2;
+
+        // primeiro número do intervalo + (numero aleatorio entre o maior e menor numero + 1);
+        numero = param1 + (rand() % (param2 + 1 - param1));
 
         printf("Seu número é o %d? \n", numero);
         printf("Digite: \n\t 0 se correto; \n\t 1 se a cima de %d; \n\t 2 se abaixo de %d; \n-> ", numero, numero);
@@ -26,11 +31,11 @@ int main()
         if (opcao == 1)
         {
             param1 = numero;
-            param2 = 100;
+            param2 = p2Anterior;
         }
         else if (opcao == 2)
         {
-            param1 = 0;
+            param1 = p1Anterior;
             param2 = numero;
         }
     } while (opcao != 0);
